78dee2ae3e23c6dc2b0ff94602595f70c73e08f9
jsebastianBeltran2002 <johan.beltran-g@mail.escuelaing.edu.co>
2026-07-07 23:28:58 -0500

    chore: initial project structure with spring initializr

---
+++ src/main/java/co/edu/escuelaing/techcup/tournament/ServiceTournamentApplication.java
@@ -0,0 +1,13 @@
+package co.edu.escuelaing.techcup.tournament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceTournamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceTournamentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/escuelaing/techcup/tournament/ServiceTournamentApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.escuelaing.techcup.tournament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceTournamentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
