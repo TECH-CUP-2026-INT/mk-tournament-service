aab52c60664b7c67c1e657ea7c72b255387025d8
Mabel Bernal <mabel.bernal-a@mail.escuelaing.edu.co>
2026-07-13 03:15:44 -0500

    feat(TC-51): eliminar torneo solo si esta Finalizado

--- src/main/java/co/edu/escuelaing/techcup/tournament/service/impl/DeleteTournamentService.java
+++ src/main/java/co/edu/escuelaing/techcup/tournament/service/impl/DeleteTournamentService.java
@@ -1,8 +1,9 @@
 package co.edu.escuelaing.techcup.tournament.service.impl;
 
 import co.edu.escuelaing.techcup.tournament.exception.TournamentNotFoundException;
-import co.edu.escuelaing.techcup.tournament.exception.TournamentNotDraftException;
+import co.edu.escuelaing.techcup.tournament.exception.TournamentCannotBeDeletedException;
 import co.edu.escuelaing.techcup.tournament.service.Tournament;
+import co.edu.escuelaing.techcup.tournament.service.TournamentStatus;
 import co.edu.escuelaing.techcup.tournament.service.ports.DeleteTournamentUseCase;
 import co.edu.escuelaing.techcup.tournament.service.ports.TournamentRepositoryPort;
 import org.springframework.stereotype.Service;
@@ -21,8 +22,8 @@ public class DeleteTournamentService implements DeleteTournamentUseCase {
         Tournament tournament = repository.findById(tournamentId)
                 .orElseThrow(() -> new TournamentNotFoundException(tournamentId));
 
-        if (!tournament.isDraft()) {
-            throw new TournamentNotDraftException(tournamentId, tournament.getStatus());
+        if (tournament.getStatus() != TournamentStatus.FINISHED) {
+            throw new TournamentCannotBeDeletedException(tournamentId, tournament.getStatus());
         }
 
         repository.deleteById(tournamentId);
